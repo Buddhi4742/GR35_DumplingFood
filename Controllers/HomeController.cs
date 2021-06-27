09481745e8296ef082ac7a36ac7e0be31e7d9549
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2021-06-27 21:05:37 +0530

    Nav try

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Dumpling_Food.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Recommended()
         {
             return View();
         }
