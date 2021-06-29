ac35d38840fc2091cb991300f731359fbe53d1e1
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2021-06-29 17:10:08 +0530

    recom start

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,3 +3,19 @@
 
 // Write your JavaScript code.
 
+$(document).ready(function () {
+$('.controls .buttons').click(function () {
+
+    $(this).addClass('button-active').siblings().removeClass('button-active');
+
+    let filter = $(this).attr('data-filter');
+    if (filter == 'all') {
+        $('.dishf .image').show(400);
+    } else {
+        $('.dishf .image').not('.' + filter).hide(200);
+        $('.dishf .image').filter('.' + filter).show(400);
+    }s
+
+});
+
+});
