2d42ac804875f6a9d35bd303d091cca462a607f3
Buddhi4742 <chathuranga4742@gmail.com>
2021-06-30 12:09:54 +0530

    commit login

---
+++ Controllers/Database/MongoCRUD.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dumpling_Food.Controllers.Database
+{
+    public class MongoCRUD
+    {
+    }
+}
