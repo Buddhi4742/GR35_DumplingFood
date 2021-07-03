8a8243bf6a6626749b506e946f6bdb36c8d725a2
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2021-07-03 11:29:08 +0530

    re try

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using Dumpling_Food.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dumpling_Food.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ILogger<CustomerController> _logger;
+
+        public CustomerController(ILogger<CustomerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Cindex()
+        {
+            return View();
+        }
+
+        public IActionResult Recommended()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User/CustomerModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dumpling_Food.Models
+{
+    public class CustomerModel
+    {
+     
+
+       
+    }
+}
