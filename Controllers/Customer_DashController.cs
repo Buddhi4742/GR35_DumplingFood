2715f3ef7167bd686c80eb22940e221335be9f88
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2021-07-07 12:17:07 +0530

    Recommended add

--- Controllers/Customer_DashController.cs
+++ Controllers/Customer_DashController.cs
@@ -28,6 +28,14 @@ namespace Dumpling_Food.Controllers
         {
             return View();
         }
+        public IActionResult Menu()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,20 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-$(document).ready(function () {
-$('.controls .buttons').click(function () {
-
-    $(this).addClass('button-active').siblings().removeClass('button-active');
-
-    let filter = $(this).attr('data-filter');
-    if (filter == 'all') {
-        $('.dishf .image').show(400);
-    } else {
-        $('.dishf .image').not('.' + filter).hide(200);
-        $('.dishf .image').filter('.' + filter).show(400);
-    }s
-
-});
-
-});
