fae659fb80da691a5a390c0067b0fd6f36648b80
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2021-07-08 21:48:45 +0530

    recent order add

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.getElementById("modal-toggle").addEventListener("click", function () {
+    document.getElementsByClassName("popup")[0].classList.add("active");
+});
+
+document.getElementById("dismiss-popup-btn").addEventListener("click", function () {
+    document.getElementsByClassName("popup")[0].classList.remove("active");
+});
