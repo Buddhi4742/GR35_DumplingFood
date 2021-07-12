f95c048f0523b1b759306ff923c7684811a023dc
osanda bhanuranga rathnayaka <47395557+osanda-b@users.noreply.github.com>
2021-07-12 19:25:19 +0530

    specials slider added
    content added

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,97 +6,98 @@
 
 
 !(function ($) {
-    "use strict";
-    // Activate smooth scroll on page load with hash links in the url
-    $(document).ready(function () {
-        if (window.location.hash) {
-            var initial_nav = window.location.hash;
-            if ($(initial_nav).length) {
-                var scrollto = $(initial_nav).offset().top - scrolltoOffset;
-                $('html, body').animate({
-                    scrollTop: scrollto
-                }, 1500, 'easeInOutExpo');
-            }
-        }
-    });
+	"use strict";
+	// Activate smooth scroll on page load with hash links in the url
+	$(document).ready(function () {
+		if (window.location.hash) {
+			var initial_nav = window.location.hash;
+			if ($(initial_nav).length) {
+				var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+				$('html, body').animate({
+					scrollTop: scrollto
+				}, 1500, 'easeInOutExpo');
+			}
+		}
+	});
 
 
-    // Toggle .header-scrolled class to #header when page is scrolled
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('#header').addClass('header-scrolled');
-            $('#topbar').addClass('topbar-scrolled');
-        } else {
-            $('#header').removeClass('header-scrolled');
-            $('#topbar').removeClass('topbar-scrolled');
-        }
-    });
+	// Toggle .header-scrolled class to #header when page is scrolled
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('#header').addClass('header-scrolled');
+			$('#topbar').addClass('topbar-scrolled');
+		} else {
+			$('#header').removeClass('header-scrolled');
+			$('#topbar').removeClass('topbar-scrolled');
+		}
+	});
 
-    if ($(window).scrollTop() > 100) {
-        $('#header').addClass('header-scrolled');
-        $('#topbar').addClass('topbar-scrolled');
-    }
+	if ($(window).scrollTop() > 100) {
+		$('#header').addClass('header-scrolled');
+		$('#topbar').addClass('topbar-scrolled');
+	}
 
-    // Real view height for mobile devices
-    if (window.matchMedia("(max-width: 767px)").matches) {
-        $('#hero').css({
-            height: $(window).height()
-        });
-    }
+	// Real view height for mobile devices
+	if (window.matchMedia("(max-width: 767px)").matches) {
+		$('#hero').css({
+			height: $(window).height()
+		});
+	}
 
 
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
+	// Back to top button
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('.back-to-top').fadeIn('slow');
+		} else {
+			$('.back-to-top').fadeOut('slow');
+		}
+	});
 
-    $('.back-to-top').click(function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 1500, 'easeInOutExpo');
-        return false;
-    });
+	$('.back-to-top').click(function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1500, 'easeInOutExpo');
+		return false;
+	});
 
-    // Menu list isotope and filter
-    $(window).on('load', function () {
-        var menuIsotope = $('.menu-container').isotope({
-            itemSelector: '.menu-item',
-            layoutMode: 'fitRows'
-        });
+	// Menu list isotope and filter
+	$(window).on('load', function () {
+		var menuIsotope = $('.menu-container').isotope({
+			itemSelector: '.menu-item',
+			layoutMode: 'fitRows'
+		});
 
-        $('#menu-flters li').on('click', function () {
-            $("#menu-flters li").removeClass('filter-active');
-            $(this).addClass('filter-active');
+		$('#menu-flters li').on('click', function () {
+			$("#menu-flters li").removeClass('filter-active');
+			$(this).addClass('filter-active');
 
-            menuIsotope.isotope({
-                filter: $(this).data('filter')
-            });
-        });
-    });
+			menuIsotope.isotope({
+				filter: $(this).data('filter')
+			});
+		});
+	});
 
-    // Testimonials carousel (uses the Owl Carousel library)
-    $(".events-carousel").owlCarousel({
-        autoplay: true,
-        dots: true,
-        loop: true,
-        items: 1
-    });
+	// Testimonials carousel (uses the Owl Carousel library)
+	$(".events-carousel").owlCarousel({
+		autoplay: true,
+		dots: true,
+		loop: true,
+		items: 1
+	});
 
-    // Testimonials carousel (uses the Owl Carousel library)
-    $(".testimonials-carousel").owlCarousel({
-        autoplay: true,
-        dots: true,
-        loop: true,
-        items: 1
-    });
+	// Testimonials carousel (uses the Owl Carousel library)
+	$(".testimonials-carousel").owlCarousel({
+		autoplay: true,
+		dots: true,
+		loop: true,
+		items: 1
+	});
 
-    // Initiate venobox lightbox
-    $(document).ready(function () {
-        $('.venobox').venobox();
-    });
+	// Initiate venobox lightbox
+	$(document).ready(function () {
+		$('.venobox').venobox();
+	});
 
 })(jQuery);
+
