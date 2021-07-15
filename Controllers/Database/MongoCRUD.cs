60c1138e1a92380828badf991e9ceb12bb379df2
Buddhi4742 <chathuranga4742@gmail.com>
2021-07-15 08:10:09 +0530

    mongo

--- Controllers/Database/MongoCRUD.cs
+++ Controllers/Database/MongoCRUD.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,12 @@ namespace Dumpling_Food.Controllers.Database
 {
     public class MongoCRUD
     {
+        private IMongoDatabase db; 
+
+        public MongoCRUD(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+        }
     }
 }
